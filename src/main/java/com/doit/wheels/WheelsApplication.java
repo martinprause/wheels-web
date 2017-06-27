c8994a0b2533054ddf671d5e116916b6fde3cd72
Denis Gorbach <max.krupenko@do-it.co>
2017-06-27 12:54:16 +0300

    [MS-1] Added tomcat deployment.

--- src/main/java/com/doit/wheels/WheelsApplication.java
+++ src/main/java/com/doit/wheels/WheelsApplication.java
@@ -4,13 +4,20 @@ import com.doit.wheels.auth.SecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Import({SecurityConfiguration.class})
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class WheelsApplication {
+public class WheelsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WheelsApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WheelsApplication.class, args);
