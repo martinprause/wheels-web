aca78eaa7f7320a8565b885e01bee87f8aa5cd4d
Denis Gorbach <denis.gorbach@do-it.co>
2017-07-26 13:26:02 +0300

    [MS-2] Fixed bug with submitting order.

--- src/main/java/com/doit/wheels/ui/nested/CommentsSubmitLayout.java
+++ src/main/java/com/doit/wheels/ui/nested/CommentsSubmitLayout.java
@@ -109,6 +109,7 @@ public class CommentsSubmitLayout extends VerticalLayout {
                         sharedBinder.getBean().setQrCode(sharedBinder.getBean().getOrderNo() + "-P");
                         sharedBinder.readBean(sharedBinder.getBean());
                         sharedBinder.validate();
+                        orderService.save(sharedBinder.getBean());
                         if (printImmediatelyCheck.getValue()){
                             for (WheelRimPosition wheelRimPosition : sharedBinder.getBean().getWheelRimPositions()) {
                                 wheelRimPosition.setQrCode(sharedBinder.getBean().getOrderNo() + "-" + wheelRimPosition.getPositionNo());
